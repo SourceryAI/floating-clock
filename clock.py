916d579000de760c53ad7388313ab9af7fb53275
ivaquero <msaintms@outlook.com>
2022-10-07 22:56:12 +0800

    build: 🎉 new beginning

--- clock.py
+++ clock.py
@@ -1,1 +1,14 @@
-﻿
+﻿import sys
+import time
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QMainWindow()
+# Windows are hidden by default.
+window.show()
+
+# Start the event loop.
+app.exec()

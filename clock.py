4a1f8720e21397f1838394087e89e3197d8cde64
ivaquero <msaintms@outlook.com>
2022-10-20 13:57:55 +0800

    build: ⬆️ update to pyside6

--- clock.py
+++ clock.py
@@ -1,14 +1,67 @@
-﻿import sys
-import time
+﻿# importing required librarie
+import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import Qt, QTime, QTimer
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
 
-app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-window = QMainWindow()
-# Windows are hidden by default.
-window.show()
+class Clock(QWidget):
 
-# Start the event loop.
-app.exec()
+    def __init__(self):
+        super().__init__()
+        # setting geometry of main window
+        self.setGeometry(40, 40, 200, 120)
+        # self.setWindowOpacity(0.5)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        # font
+        font = QFont()
+        font.setFamily("Arial")
+        font.setPointSize(60)
+        font.setBold(True)
+
+        # label object
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color:rgb(0,0,0);")
+        # self.label.setAutoFillBackground(True)
+
+        # layout
+        layout = QVBoxLayout()
+        # add label to the layout
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+        # timer object
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        # update the timer every second
+        timer.start(1000)
+
+        # button
+
+    # method called by timer
+    def showTime(self):
+        # getting current time
+        current_time = QTime.currentTime()
+        # converting QTime object to string
+        label_time = current_time.toString('hh:mm')
+
+        # show it to the label
+        self.label.setText(label_time)
+
+
+if __name__ == '__main__':
+    # app
+    App = QApplication(sys.argv)
+    # window
+    clock = Clock()
+
+    # show all the widgets
+    clock.show()
+
+    # start the app
+    App.exit(App.exec())

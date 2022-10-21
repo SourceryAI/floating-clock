8066ad9629fefc9316e8c8ab8eee636f0973aedf
ivaquero <msaintms@outlook.com>
2022-10-21 20:05:07 +0800

    build: 🏗️ update ui files

--- clock/ui.py
+++ clock/ui.py
@@ -2,14 +2,15 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QWidget
-
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_ui
 
+
 class ui(QWidget):
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_ui()

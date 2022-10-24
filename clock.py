e4ae174978eaff781322c78007627fbcfb2a6446
ivaquero <msaintms@outlook.com>
2022-10-24 11:29:55 +0800

    build: 🔧 update right click menu

--- clock.py
+++ clock.py
@@ -1,7 +1,7 @@
 ﻿import sys
 
 from PySide6.QtCore import QPoint, Qt, QTime, QTimer
-from PySide6.QtGui import QAction, QCursor, QFont, QMouseEvent
+from PySide6.QtGui import QAction, QFont, QMouseEvent
 from PySide6.QtWidgets import QApplication, QLabel, QMenu, QVBoxLayout, QWidget
 
 
@@ -42,11 +42,55 @@ class Clock(QWidget):
 
     def mouseRightMenu(self, pos) -> None:
         self.menu = QMenu(self)
-        self.actionA = QAction('fontsize', self)
-        self.menu.addAction(self.actionA)
+        fontSizeMenu = self.menu.addMenu('font size')
+        self.actionA1 = QAction('larger', self)
+        self.actionA2 = QAction('smaller', self)
+        fontSizeMenu.addAction(self.actionA1)
+        fontSizeMenu.addAction(self.actionA2)
 
-        self.actionB = QAction('fontcolor', self)
-        self.menu.addAction(self.actionB)
+        fontColorMenu = self.menu.addMenu('font color')
+        self.actionB1 = QAction('black', self)
+        self.actionB2 = QAction('red', self)
+        self.actionB3 = QAction('yellow', self)
+        self.actionB4 = QAction('blue', self)
+        self.actionB5 = QAction('orange', self)
+        self.actionB6 = QAction('green', self)
+        self.actionB7 = QAction('white', self)
+        fontColorMenu.addAction(self.actionB1)
+        fontColorMenu.addAction(self.actionB2)
+        fontColorMenu.addAction(self.actionB3)
+        fontColorMenu.addAction(self.actionB4)
+        fontColorMenu.addAction(self.actionB5)
+        fontColorMenu.addAction(self.actionB6)
+        fontColorMenu.addAction(self.actionB7)
+
+        fontOpacityMenu = self.menu.addMenu('font opacity')
+        self.actionC1 = QAction('increase', self)
+        self.actionC2 = QAction('decrease', self)
+        fontOpacityMenu.addAction(self.actionC1)
+        fontOpacityMenu.addAction(self.actionC2)
+
+        bgColorMenu = self.menu.addMenu('bg color')
+        self.actionD1 = QAction('black', self)
+        self.actionD2 = QAction('red', self)
+        self.actionD3 = QAction('yellow', self)
+        self.actionD4 = QAction('blue', self)
+        self.actionD5 = QAction('orange', self)
+        self.actionD6 = QAction('green', self)
+        self.actionD7 = QAction('white', self)
+        bgColorMenu.addAction(self.actionD1)
+        bgColorMenu.addAction(self.actionD2)
+        bgColorMenu.addAction(self.actionD3)
+        bgColorMenu.addAction(self.actionD4)
+        bgColorMenu.addAction(self.actionD5)
+        bgColorMenu.addAction(self.actionD6)
+        bgColorMenu.addAction(self.actionD7)
+
+        bgOpacityMenu = self.menu.addMenu('bg opacity')
+        self.actionE1 = QAction('increase', self)
+        self.actionE2 = QAction('decrease', self)
+        bgOpacityMenu.addAction(self.actionE1)
+        bgOpacityMenu.addAction(self.actionE2)
 
         self.menu.exec(self.mapToGlobal(pos))
 
